0769a18db1741b2713198509eedc3fde43745bc2
Ren0g <98395283+Ren0g@users.noreply.github.com>
2025-11-27 11:50:58 +0100

    Update route.js

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,3 +1,6 @@
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 import nodemailer from "nodemailer";
 
 export async function POST(req) {
@@ -5,21 +8,21 @@ export async function POST(req) {
     const body = await req.json();
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.zoho.eu",   // ako si na US datacentru, koristi smtp.zoho.com
+      host: "smtp.zoho.eu",
       port: 587,
       secure: false,
       auth: {
         user: "info@promar.hr",
-        pass: "BxzxeMJJrd38"
+        pass: "TVOJ_APP_PASSWORD"
       }
     });
 
     await transporter.sendMail({
-      from: 'Promar <info@promar.hr>',
-      to: 'info@promar.hr',
+      from: "Promar <info@promar.hr>",
+      to: "info@promar.hr",
       subject: "Nova poruka sa Promar.hr",
       text: `
-Ime i prezime: ${body.name}
+Ime: ${body.name}
 Email: ${body.email}
 Mobitel: ${body.phone}
 Usluga: ${body.service}
@@ -30,9 +33,11 @@ ${body.message}
     });
 
     return Response.json({ success: true }, { status: 200 });
-
   } catch (err) {
     console.error("MAIL ERROR:", err);
-    return Response.json({ success: false, error: err.message }, { status: 500 });
+    return Response.json(
+      { success: false, error: err.message },
+      { status: 500 }
+    );
   }
 }
